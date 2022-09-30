aa5c27b970a575ac341d3f2d205c0ea69650c8c3
lahanatou <nouryous30@gmail.com>
2022-09-30 01:48:41 +0100

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'tasks#index'
+  #root 'tasks#index'
+  root 'users#new'
   resources :tasks
+  resources :sessions, only: [:new, :create, :destroy]
+   resources :users, only: [:new, :create, :show]
 end
